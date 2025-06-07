476595b7cc80baeb5309ca941bd06d748d688c41
Damidesousa <damidesousa@gmail.com>
2025-06-06 22:52:30 -0300

    agegando una pregunta como usuario

--- controller/CrearPreguntaController.php
+++ controller/CrearPreguntaController.php
@@ -14,4 +14,16 @@ class CrearPreguntaController
     public function show() {
         $this->view->render("CrearPregunta");
     }
+    public function agregar(){
+        $pregunta = $_POST['pregunta'];
+        $Categoria = $_POST['Categoria'];
+        $Opcion1 = $_POST['Opcion1'];
+        $Opcion2 = $_POST['Opcion2'];
+        $Opcion3 = $_POST['Opcion3'];
+        $Opcion4 = $_POST['Opcion4'];
+        $esCorrecta = $_POST['esCorrecta'];
+        $this->model->agregarPregunta($pregunta,$Categoria);
+        $this->model->agregarOpciones($Opcion1,$Opcion2,$Opcion3,$Opcion4,$esCorrecta);
+        $this->view->render("PreguntaAVerificar");
+    }
 }
--- model/CrearPreguntaModel.php
+++ model/CrearPreguntaModel.php
@@ -14,4 +14,46 @@ class CrearPreguntaModel
         $result = $this->db->query($sql);
         return $result[0]["id_mas_alto"];
     }
+    public function agregarPregunta($pregunta,$categoria){
+        $sql="INSERT INTO preguntas (pregunta,estado,verificado,id_usuario,id_categoria) VALUES ('$pregunta',0,1,1,'$categoria')";
+        $this->db->query($sql);
+    }
+    public function agregarOpciones($opcion1,$opcion2,$opcion3,$opcion4,$esCorrecta){
+        $preguntaID=$this->conseguirIdMasAltoPregunta();
+        switch($esCorrecta){
+            case 1:
+                $sql="INSERT INTO opciones(pregunta_id,opcion,es_correcta)
+                        VALUES ($preguntaID,'$opcion1',1),
+                               ($preguntaID,'$opcion2',0),
+                               ($preguntaID,'$opcion3',0),
+                               ($preguntaID,'$opcion4',0)";
+                $this->db->query($sql);
+                break;
+            case 2:
+                $sql="INSERT INTO opciones(pregunta_id,opcion,es_correCTA)
+                        VALUES ($preguntaID,'$opcion1',0),
+                               ($preguntaID,'$opcion2',1),
+                               ($preguntaID,'$opcion3',0),
+                               ($preguntaID,'$opcion4',0)";
+                $this->db->query($sql);
+                break;
+            case 3:
+                $sql="INSERT INTO opciones(pregunta_id,opcion,es_correcta)
+                        VALUES ($preguntaID,'$opcion1',0),
+                               ($preguntaID,'$opcion2',0),
+                               ($preguntaID,'$opcion3',1),
+                               ($preguntaID,'$opcion4',0)";
+                $this->db->query($sql);
+                break;
+            case 4:
+                $sql="INSERT INTO opciones(pregunta_id,opcion,es_correcta)
+                       VALUES ($preguntaID,'$opcion1',0),
+                              ($preguntaID,'$opcion2',0),
+                              ($preguntaID,'$opcion3',0),
+                              ($preguntaID,'$opcion4',1)";
+                $this->db->query($sql);
+                break;
+
+        }
+    }
 }
